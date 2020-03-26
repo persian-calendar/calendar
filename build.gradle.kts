595e0e6561d43596f1d843d97b7237f459cd48d9
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2020-03-26 06:40:01 +0430

    Use gradle kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.71"
+    `maven-publish`
+}
+
+group = "io.github.persian-clander"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/persian-calendar/calendar")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("PASSWORD")
+            }
+        }
+    }
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+        }
+    }
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+    testImplementation(kotlin("stdlib-jdk8"))
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
