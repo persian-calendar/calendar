7addf336e2861564822452e257275bb7b2b307fc
Ebrahim Byagowi <ebrahim@gnu.org>
2021-10-19 12:49:43 +0330

    Add officially declared as leap years of Persian calendar test

--- src/test/java/io/github/persiancalendar/calendar/ImportedTests.kt
+++ src/test/java/io/github/persiancalendar/calendar/ImportedTests.kt
@@ -6,6 +6,26 @@ import org.junit.Test
 class ImportedTests {
 
     @Test
+    fun `Conforms with officially published leap years`() {
+        // Doesn't match with https://calendar.ut.ac.ir/Fa/News/Data/Doc/KabiseShamsi1206-1498-new.pdf
+        val leapYears = listOf(
+            1210, 1214, 1218, 1222, 1226, 1230, 1234, 1238, 1243, 1247, 1251, 1255, 1259, 1263,
+            1267, 1271, 1276, 1280, 1284, 1288, 1292, 1296, 1300, 1304, 1309, 1313, 1317, 1321,
+            1325, 1329, 1333, 1337, 1342, 1346, 1350, 1354, 1358, 1362, 1366, 1370, 1375, 1379,
+            1383, 1387, 1391, 1395, 1399, 1403, 1408, 1412, 1416, 1420, 1424, 1428, 1432, 1436,
+            1441, 1445, 1449, 1453, 1457, 1461, 1465, 1469, 1474, 1478, 1482, 1486, 1490, 1494,
+            1498
+        )
+
+        (1206..1498).forEach {
+            assertEquals(
+                it.toString(), if (it in leapYears) 366 else 365,
+                PersianDate(it + 1, 1, 1).toJdn() - PersianDate(it, 1, 1).toJdn()
+            )
+        }
+    }
+
+    @Test
     fun `Partially conforming with calendariale tests`() {
         val J0000 = 1721425L // Ours is different apparently
         listOf(
