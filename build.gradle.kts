3d809db4ef52185ad83eac6ce7717296186bb3ec
Ebrahim Byagowi <ebrahim@gnu.org>
2022-03-11 11:28:46 +0330

    Reuse JUnit 5 versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    val junit5Version = "5.8.2"
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junit5Version")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junit5Version")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junit5Version")
     testImplementation(kotlin("stdlib-jdk8"))
 }
 
