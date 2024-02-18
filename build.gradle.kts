e41b3e0587a59214b9f8d6deafdd2b2e79a0ea5f
Ebrahim Byagowi <ebrahim@gnu.org>
2024-02-18 13:27:17 +0330

    Remove unused configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,16 +29,6 @@ configure<JavaPluginExtension> {
 }
 
 publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/persian-calendar/calendar")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("PASSWORD")
-            }
-        }
-    }
     publications {
         register("mavenJava", MavenPublication::class) {
             from(components["java"])
