cb5b3e7e47246f19fea81b2e87775ad9fdd45d12
Ebrahim Byagowi <ebrahim@gnu.org>
2025-03-27 13:53:17 +0330

    test: Run ettelaat newspaper tests

--- src/test/kotlin/io/github/persiancalendar/calendar/QamariTests.kt
+++ src/test/kotlin/io/github/persiancalendar/calendar/QamariTests.kt
@@ -1,33 +1,50 @@
 package io.github.persiancalendar.calendar
 
 import io.github.persiancalendar.calendar.islamic.IranianIslamicDateConverter
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvFileSource
-import org.junit.jupiter.params.provider.CsvSource
-import org.junit.jupiter.params.provider.CsvSources
+import org.junit.jupiter.api.assertAll
+import kotlin.test.Ignore
+import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class QamariTests {
 
-    private val calendarCenterGroundTruth = IranianIslamicDateConverter::class.java
-        .getResourceAsStream("/qamari/calendar-center.txt")
-        ?.readBytes()!!
-        .decodeToString()
-        .split("\n")
-        .map { it.split("#")[0] }
-        .filter { it.isNotBlank() }
-        .map { it.trimStart('*').trim() }
-        .map { it.replace(Regex("[ /-]"), ",") }
-        .map { line -> line.split(',').map { it.toInt() } }
-
     @Test
-    fun `Conforms with calendar center data`() {
-        calendarCenterGroundTruth.forEach { (qamariYear, qamariMonth, year, month, day) ->
-            assertEquals(
-                IslamicDate(qamariYear, qamariMonth, 1).toJdn(),
-                CivilDate(year, month, day).toJdn(),
-            )
-        }
+    fun `Conforms with calendar center data`() = assertAll(prepareRunners("calendar-center.txt"))
+
+    @Ignore
+    @Test
+    fun `Conforms with ettelaat data`() = assertAll(prepareRunners("ettelaat.txt"))
+
+    private fun prepareRunners(testFileName: String): List<() -> Unit> {
+        return IranianIslamicDateConverter::class.java
+            .getResourceAsStream("/qamari/$testFileName")
+            ?.readBytes()
+            .let {
+                if (it == null) {
+                    System.err.println("$testFileName couldn't be found, skip")
+                    ByteArray(0)
+                } else it
+            }
+            .decodeToString()
+            .split("\n")
+            .map { it.split("#")[0] }
+            .filter { it.isNotBlank() }
+            .map { line ->
+                val (qamariYear, qamariMonth, year, month, day) = (
+                        line
+                            .trimStart('*').trim()
+                            .replace(Regex("[ /-]"), ",")
+                            .split(',')
+                            .map { it.toInt() })
+                {
+                    assertEquals(
+                        listOf(year, month, day),
+                        CivilDate(IslamicDate(qamariYear, qamariMonth, 1)).let { date ->
+                            listOf(date.year, date.month, date.dayOfMonth)
+                        },
+                        line.split(" ")[0]
+                    )
+                }
+            }
     }
 }
