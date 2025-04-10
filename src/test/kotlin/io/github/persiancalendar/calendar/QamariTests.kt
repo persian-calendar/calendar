961234840cc55a3c05789c0007152cba73400757
Ebrahim Byagowi <ebrahim@gnu.org>
2025-04-10 11:11:40 +0330

    Add best effort Qamari predictions
    Provided by Roozbeh Pournader as just in case to not
    be too wrong when the app isn't updated.

--- src/test/kotlin/io/github/persiancalendar/calendar/QamariTests.kt
+++ src/test/kotlin/io/github/persiancalendar/calendar/QamariTests.kt
@@ -9,37 +9,42 @@ import kotlin.test.fail
 class QamariTests {
 
     @Test
-    fun `Conforms with Qamari tests`() {
+    fun `Conforms with Qamari tests`() = checkFile("/qamari/consolidated.txt")
+
+    @Test
+    fun `Conforms with predictions`() = checkFile("/qamari-perdictions.txt")
+
+    fun checkFile(fileName: String) {
         assertAll(
             IranianIslamicDateConverter::class.java
-                .getResourceAsStream("/qamari/consolidated.txt")
-            ?.readBytes()
-            .let {
-                if (it == null) {
-                    System.err.println("consolidated.txt couldn't be found, skip")
-                    ByteArray(0)
-                } else it
-            }
-            .decodeToString()
-            .split("\n")
-            .map { it.split("#")[0] }
-            .filter { it.isNotBlank() }
-            .map { line ->
-                val (qamariYear, qamariMonth, year, month, day) = (
-                        line
-                            .trimStart('*').trim()
-                            .replace(Regex("[ /-]"), ",")
-                            .split(',')
-                            .map { it.toIntOrNull() ?: fail(line) })
-                {
-                    assertEquals(
-                        listOf(year, month, day),
-                        CivilDate(IslamicDate(qamariYear, qamariMonth, 1)).let { date ->
-                            listOf(date.year, date.month, date.dayOfMonth)
-                        },
-                        line.split(" ")[0]
-                    )
+                .getResourceAsStream(fileName)
+                ?.readBytes()
+                .let {
+                    if (it == null) {
+                        System.err.println("consolidated.txt couldn't be found, skip")
+                        ByteArray(0)
+                    } else it
                 }
-            })
+                .decodeToString()
+                .split("\n")
+                .map { it.split("#")[0] }
+                .filter { it.isNotBlank() }
+                .map { line ->
+                    val (qamariYear, qamariMonth, year, month, day) = (
+                            line
+                                .trimStart('*').trim()
+                                .replace(Regex("[ /-]"), ",")
+                                .split(',')
+                                .map { it.toIntOrNull() ?: fail(line) })
+                    {
+                        assertEquals(
+                            listOf(year, month, day),
+                            CivilDate(IslamicDate(qamariYear, qamariMonth, 1)).let { date ->
+                                listOf(date.year, date.month, date.dayOfMonth)
+                            },
+                            fileName + ": " + line.split(" ")[0]
+                        )
+                    }
+                })
     }
 }
