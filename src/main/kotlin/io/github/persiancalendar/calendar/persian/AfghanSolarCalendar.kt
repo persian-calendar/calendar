a0b21fff775d569c24a1ff14c7d6802c9eb5e62a
Ebrahim <ebrahim@gnu.org>
2026-05-29 15:53:07 +0330

    Add Afghanistan's non-fixed solar calendar months data
    Not used yet

---
+++ src/main/kotlin/io/github/persiancalendar/calendar/persian/AfghanSolarCalendar.kt
@@ -0,0 +1,21 @@
+package io.github.persiancalendar.calendar.persian;
+
+// https://iranicaonline.org/uploads/files/Calendars/v4f6a073_t20_300.jpg
+val months = listOf(
+    // Lunar 1322 -> 1283
+    //listOf(31, 30, 32, 31, 31, 32, 30, 30, 29, 30, 29, 30),
+    // 1312
+    listOf(31, 31, 31, 31, 32, 31, 30, 30, 29, 30, 29, 30),
+    // 1313
+    listOf(31, 31, 31, 32, 31, 31, 31, 30, 29, 29, 30, 30),
+    // 1314
+    listOf(31, 31, 31, 32, 31, 31, 30, 29, 30, 29, 30, 30),
+    // 1315
+    listOf(31, 31, 32, 31, 31, 31, 30, 30, 29, 30, 29, 30),
+    // 1316
+    listOf(31, 31, 31, 32, 31, 31, 30, 30, 29, 29, 30, 30),
+    // 1317
+    listOf(31, 31, 32, 31, 31, 31, 30, 30, 29, 30, 30, 30),
+    // from this point just use the modern variant of months, 1336
+    // listOf(31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29),
+)
